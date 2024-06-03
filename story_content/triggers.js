11b3a0c7f1ebc3be20a20c0a8dd181be94c43f38
dojoearth <162415782+dojoearth@users.noreply.github.com>
2024-06-03 10:54:03 +0800

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,33 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5zNEdEhaQaZ":
+        Script1();
+        break;
+      case "5lfZI4wED7i":
+        Script2();
+        break;
+      case "5bx3aRfKRbz":
+        Script3();
+        break;
+      case "6Bj8EoyTV82":
+        Script4();
+        break;
+      case "6nUYsxBJGnT":
+        Script5();
+        break;
+      case "5V8XM6P1Zht":
+        Script6();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
